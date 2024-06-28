d29a319db6851a20dbd907fa7748a011a510fa12
kaizerxz3 <rinatuului@gmail.com>
2024-06-28 16:42:15 +0600

    add second page

---
+++ Second_Page/secondjs.js
@@ -0,0 +1,12 @@
+// let ul = document.querySelector('ulli ul')
+
+// let showBurger = ()=>{
+//   ul.classList.toggle('active')
+// }   
+
+let popup = querySelector('.popup')
+
+let popupToggle = () => {
+
+    popup.classList.toogle('active')
+}
